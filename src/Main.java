3c066cb07c75bf160048669dc1b349f9f11a3fb7
ysh020702 <ysh020702@gmail.com>
2024-09-09 19:42:04 +0900

    new_file

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
         System.out.printf("Hello and welcome!");
 
+        int new_method = 10039;
+        System.out.println(new_method);
+
         for (int i = 1; i <= 5; i++) {
             //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
             // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
