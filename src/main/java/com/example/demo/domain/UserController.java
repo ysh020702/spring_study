7c71d9cf76a63dabbadb6c4391f1d9810d73ba17
ysh020702 <ysh020702@gmail.com>
2024-09-25 18:46:33 +0900

    4차시 USER JAVA FILE

---
+++ src/main/java/com/example/demo/domain/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<UserSimpleResponseDto> registerUser(@RequestBody UserRegistrationRequestDto userRegistrationRequestDto) {
+        User user = new User(
+                userRegistrationRequestDto.getUsername(),
+                userRegistrationRequestDto.getPassword(),
+                userRegistrationRequestDto.getName()
+        );
+        UserSimpleResponseDto savedUser = userService.saveUser(user);
+
+        return ResponseEntity.ok(savedUser);
+    }
+}
